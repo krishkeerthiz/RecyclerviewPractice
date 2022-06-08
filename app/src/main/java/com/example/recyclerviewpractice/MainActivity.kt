bc378372edd6c2bd94635ea591244066f88a04c2
keerthi-con927 <keerthik97117@gmail.com>
2022-06-08 15:51:29 +0530

    notify item *, diff utils, get item view type

--- app/src/main/java/com/example/recyclerviewpractice/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewpractice/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.recyclerviewpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerviewpractice.databinding.ActivityMainBinding
@@ -16,7 +17,8 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        val items = listOf("text -1","text 0","text 1","text 2","text 3","text 4","text 5","text 6","text 7","text 8","text 9","text 10")
+
+        val items = getSampleArrayList()
         val rv = findViewById<RecyclerView>(R.id.recycler_view)
         val rvAdapter = RvAdapter(items)
 
@@ -24,8 +26,92 @@ class MainActivity : AppCompatActivity() {
             layoutManager = LinearLayoutManager(context)
             adapter = rvAdapter
             setItemViewCacheSize(1)
-
+            //rvAdapter.submitList(items) need to submit list while using list adapter
+            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL)) // adds line between views
         }
 
+//        val items = mutableListOf("text -1","text 0","text 1","text 2","text 3","text 4","text 5","text 6","text 7","text 8","text 9","text 10")
+//        rv.apply {
+//            layoutManager = LinearLayoutManager(context)
+//            adapter = rvAdapter
+//            setItemViewCacheSize(1)
+//            //rvAdapter.submitList(items) need to submit list while using list adapter
+//            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL)) // adds line between views
+//        }
+
+//        binding.button.setOnClickListener {
+//            items.add("Text 11")
+//            rvAdapter.notifyDataSetChanged()
+//        }
+
+//        binding.button.setOnClickListener {
+//            items.add("Text 11")
+//            rvAdapter.notifyItemInserted(items.size - 1)
+//            rv.scrollToPosition(items.size - 1)  // auto scrolls to the inserted item position
+//            //scrolling is smooth
+//        }
+
+//        binding.button.setOnClickListener {
+//            items.add(0, "Text -2")   // insertion starts from 0
+//            rvAdapter.notifyItemInserted(0)
+//            rv.smoothScrollToPosition(0)  // auto scrolls to the inserted item position
+//            //scrolling is smooth now
+//        }
+
+//        binding.button.setOnClickListener {
+//            val position = items.size - 1
+//            items.removeAt(position)
+//            rvAdapter.notifyItemRemoved(position) // position from which item is removed
+//            rv.smoothScrollToPosition(16)  // giving scroll position beyond size does not cause any issue
+//            //scrolling is smooth now
+//        }
+
+//        binding.button.setOnClickListener {
+//            val position = items.size - 1
+//            items[position] = "Text 999"
+//            rvAdapter.notifyItemChanged(position) // position from which item is removed
+//            rv.smoothScrollToPosition(position)  // from start of the items, scrolling to the end updates ui without
+//            // notify item changed being called, note: this wont work when the item to changed is visible
+//        }
+
+//        binding.button.setOnClickListener {
+//            val position = items.size
+//            items.addAll(listOf("Text 11", "Text 12", "Text 13"))
+//            rvAdapter.notifyItemRangeInserted(position, 3)
+//            rv.scrollToPosition(position)  // shows first added item at the end of the screen we need to scroll to see more added items
+//        }
+
+
+        // List adapter
+//        binding.button.setOnClickListener {
+//            val position = items.size
+//            items.addAll(listOf("Text 11", "Text 12", "Text 13"))
+//            rvAdapter.submitList(items) // DiffUtil takes care of the check
+//            rv.scrollToPosition(position)  // shows first added item at the end of the screen we need to scroll to see more added items
+//        }
+
+//        binding.button.setOnClickListener {
+//            val position = items.size
+//            items.addAll(listOf("Text 11", "Text 12", "Text 13"))
+//            rvAdapter.updateItems(items)
+//            rv.scrollToPosition(position)  // shows first added item at the end of the screen we need to scroll to see more added items
+//            // Smoothscrolltopoisition is not working in diffutils
+//        }
+
+       // LinearSnapHelper().attachToRecyclerView(rv) // works for horizontal
+    }
+
+
+    private fun getSampleArrayList(): MutableList<Any> {
+        val items: MutableList<Any> = mutableListOf()
+        items.add(User("Dany Targaryen", "Valyria"))
+        items.add(User("Rob Stark", "Winterfell"))
+        items.add("Walter White")
+        items.add(User("Jon Snow", "Castle Black"))
+        items.add("Michael scofield")
+        items.add(User("Tyrion Lanister", "King's Landing"))
+        return items
     }
 }
+
+data class User(val firstName: String, val lastName: String)
--- app/src/main/java/com/example/recyclerviewpractice/RvAdapter.kt
+++ app/src/main/java/com/example/recyclerviewpractice/RvAdapter.kt
@@ -5,27 +5,150 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewpractice.databinding.RvView2Binding
 import com.example.recyclerviewpractice.databinding.RvViewBinding
 
-class RvAdapter(private val data: List<String>) : RecyclerView.Adapter<RvViewHolder>() {
+class RvAdapter(private var data: MutableList<Any>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RvViewHolder {
+    private val VIEW1 = 1
+    private val VIEW2 = 2
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         Log.d(TAG, "onCreateViewHolder: Recyclerview")
+        val viewHolder: RecyclerView.ViewHolder
         val view = LayoutInflater.from(parent.context)
-        val binding = RvViewBinding.inflate(view, parent, false)
-        return RvViewHolder(binding)
+
+        viewHolder = when (viewType) {
+            VIEW1 -> {
+                val binding = RvViewBinding.inflate(view, parent, false)
+                RvViewHolder(binding)
+            }
+            VIEW2 -> {
+                val binding = RvView2Binding.inflate(view, parent, false)
+                RvViewHolder2(binding)
+            }
+            else -> {
+                val binding = RvViewBinding.inflate(view, parent, false)
+                RvViewHolder(binding)
+            }
+        }
+
+        return viewHolder
+//        val binding = RvViewBinding.inflate(view, parent, false)
+//        return RvViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: RvViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         Log.d(TAG, "onBindViewHolder: Recyclerview ${data[position]}")
-        holder.bind(data[position])
+        when (holder.itemViewType) {
+            VIEW1 -> {
+                val vh1 = holder as RvViewHolder
+                vh1.bind(data[position] as String)
+            }
+            VIEW2 -> {
+                val vh2 = holder as RvViewHolder2
+                vh2.bind(data[position] as User)
+            }
+            else -> {
+                val vh1 = holder as RvViewHolder
+                vh1.bind(data[position] as String)
+            }
+        }
     }
 
     override fun getItemCount():Int {
-        Log.d(TAG, "getItemCount: Recyclerview ${data.size}")
+        Log.d(TAG, "getItemCount: Recyclervie ${data.size}")
         return data.size
     }
+
+    override fun getItemViewType(position: Int): Int {
+        return when(data[position]){
+            is String -> 1
+            is User -> 2
+            else -> -1
+        }
+    }
+
 }
+
+//class RvAdapter(private var data: List<String>) : RecyclerView.Adapter<RvViewHolder>() {
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RvViewHolder {
+//        Log.d(TAG, "onCreateViewHolder: Recyclerview")
+//        val view = LayoutInflater.from(parent.context)
+//        val binding = RvViewBinding.inflate(view, parent, false)
+//        return RvViewHolder(binding)
+//    }
+//
+//    override fun onBindViewHolder(holder: RvViewHolder, position: Int) {
+//        Log.d(TAG, "onBindViewHolder: Recyclerview ${data[position]}")
+//        holder.bind(data[position])
+//    }
+//
+//    override fun getItemCount():Int {
+//        Log.d(TAG, "getItemCount: Recyclervie ${data.size}")
+//        return data.size
+//    }
+//
+//    fun updateItems(newData: List<String>){
+//        val diffCallback = DiffCallback(data, newData)
+//        val diffResult = DiffUtil.calculateDiff(diffCallback)
+//
+//        data = newData
+//
+//        diffResult.dispatchUpdatesTo(this) // calls adapter's notify methods after diff is computed
+//    }
+//}
+
+//class DiffCallback(private val oldItems: List<String>, private val newItems: List<String>)
+//    : DiffUtil.Callback(){
+//    override fun getOldListSize() = oldItems.size
+//
+//    override fun getNewListSize() = newItems.size
+//
+//    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+//        return oldItems[oldItemPosition] == newItems[newItemPosition]
+//    }
+//
+//    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+//        return oldItems[oldItemPosition] == newItems[newItemPosition]
+//    }
+//
+//}
+
+//class RvAdapter(private val data: List<String>) : ListAdapter<String, RvViewHolder>(DIFF_CALLBACK) {
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RvViewHolder {
+//        Log.d(TAG, "onCreateViewHolder: Recyclerview")
+//        val view = LayoutInflater.from(parent.context)
+//        val binding = RvViewBinding.inflate(view, parent, false)
+//        return RvViewHolder(binding)
+//    }
+//
+//    override fun onBindViewHolder(holder: RvViewHolder, position: Int) {
+//        Log.d(TAG, "onBindViewHolder: Recyclerview ${data[position]}")
+//        holder.bind(getItem(position))
+//    }
+//
+////    override fun getItemCount():Int {
+////        Log.d(TAG, "getItemCount: Recyclerview ${data.size}")
+////        return data.size
+////    }
+//
+//    companion object{
+//        val DIFF_CALLBACK =  object: DiffUtil.ItemCallback<String>(){
+//            override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+//                return oldItem == newItem
+//            }
+//
+//            override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+//                return oldItem == newItem
+//            }
+//
+//        }
+//    }
+//}
+
 
 class RvViewHolder(private val binding: RvViewBinding): RecyclerView.ViewHolder(binding.root) {
 
@@ -33,3 +156,24 @@ class RvViewHolder(private val binding: RvViewBinding): RecyclerView.ViewHolder(
         binding.textView.text = data
     }
 }
+
+class RvViewHolder2(private val binding: RvView2Binding): RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(data: User){
+        binding.textView.text = data.firstName
+        binding.textView2.text = data.lastName
+    }
+}
+
+//Note:
+//fun updateItems(newData: List<String>){
+//
+//    val diffCallback = DiffCallback(listOf("198"), listOf("Text 17"))
+//    Log.d(TAG, "updateItems: ${data.size}  ${newData.size}")
+//    Log.d(TAG, "updateItems: ${data.hashCode()}  ${newData.hashCode()}")  // both hashcodes are same, but data varies
+//    val diffResult = DiffUtil.calculateDiff(diffCallback)
+//
+//    //data = newData
+//
+//    diffResult.dispatchUpdatesTo(this) // calls adapter's notify methods after diff is computed
+//}
